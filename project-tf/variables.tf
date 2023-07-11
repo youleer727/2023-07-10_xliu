1dffc25b3d439840a4e76f805b473efbee8532d8
youleer727 <danicaliu727@gmail.com>
2023-07-11 20:25:03 +0000

    terraform modules

---
+++ project-tf/variables.tf
@@ -0,0 +1,16 @@
+/* Understanding Modules
+   variables.tf - part of the "root module", this file defines variables that would be used within main.tf in the root module.
+                  within a cloud module, we might want to have something like a region mapped as a variable in the root main.tf */
+
+variable "container_name_from_root" {
+  description = "name of the container to apply"
+  type        = string
+  default     = "defaultContainerNamefromRootModule" # override this if you want to change the name used by the child module
+}
+
+variable "external_port_from_root" { 
+  description = "external port to apply"
+  type        = string
+  default     = 2000
+}
+
